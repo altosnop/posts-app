f9309c2df6fe571953818d5c6a20e1e2a7f43ccb
Alex Lar <shimanuchi@gmail.com>
2022-10-18 22:27:17 +0300

    created simple page with dynamic posts

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,33 @@
+import axios from 'axios';
+
+import cardTpl from '../templates/card.handlebars';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../css/styles.css';
+
+let data = {};
+
+const refs = {
+  cardsEl: document.querySelector('.cards'),
+  addBtnEl: document.querySelector('.add-btn'),
+};
+
+getData().then((res) => {
+  data = res;
+  const cards = cardTpl(data.posts);
+  insertCardToHTML(cards);
+});
+
+async function getData() {
+  const posts = await axios.get('https://jsonplaceholder.typicode.com/posts');
+  const users = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+  return {
+    posts: posts.data,
+    users: users.data,
+  };
+}
+
+function insertCardToHTML(card) {
+  refs.cardsEl.insertAdjacentHTML('beforeend', card);
+}
